0cc13aae8508128e2deaa7c02a97ca572998245e
Tune <cfdw011@gmail.com>
2021-11-12 20:56:53 +0900

    golang.or document tutiorial import modules & use

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
+	"rsc.io/quote"
+)
+
+func plus(a, b int, name string) (int, string) {
+	return a + b, name
+}
+
+func sum(a ...int) int {
+	var total int
+	for _, el := range a {
+		total += el
+	}
+	return total
+}
 func main() {
-	fmt.Println("It works")
+	fmt.Println(quote.Go())
+	result, name := plus(1, 2, "Ant")
+	fmt.Println(result)
+	fmt.Println(name)
+
+	sum := sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 1100)
+	fmt.Println(sum)
 }
