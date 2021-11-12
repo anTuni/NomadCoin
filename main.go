8fb0f6743d0371119ac4b7d169ade8386480726c
Tune <cfdw011@gmail.com>
2021-11-12 22:05:32 +0900

    #3.5 pointer

--- main.go
+++ main.go
@@ -1,45 +1,14 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"rsc.io/quote"
-)
-
-func plus(a, b int, name string) (int, string) {
-	return a + b, name
-}
-
-func sum(a ...int) int {
-	var total int
-	for _, el := range a {
-		total += el
-	}
-	return total
-}
 func main() {
-	fmt.Println(quote.Go())
-	result, name := plus(1, 2, "Ant")
-	fmt.Println(result)
-	fmt.Println(name)
+	a := 1234
+	b := a
+	c := &a
+	d := *&a
 
-	sum := sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 1100)
-	fmt.Println(sum)
-
-	foods := [3]string{"kimchi", "sam", "chicken"}
-
-	for _, food := range foods {
-		fmt.Println(food)
-	}
-
-	people := []string{"Antuni", "JJM", "Beast"}
-
-	for i := 0; i < len(people); i++ {
-		fmt.Println(people[i])
-	}
-	more := append(people, "JK")
-
-	println(more[0], more[1], more[2], more[3])
-	println(len(more))
-	println(len(people))
+	fmt.Println(a, "\n", b, "\n", c, "\n", d, "\n", *c)
+	a = 4321
+	fmt.Println(a, "\n", b, "\n", c, "\n", d, "\n", *c)
 }
