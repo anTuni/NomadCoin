1055229373af1e7192d398d9c49b71f8fb338e96
Tune <cfdw011@gmail.com>
2021-11-20 10:58:03 +0900

    5.5 refactoring server code

--- main.go
+++ main.go
@@ -1,48 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"text/template"
-
-	"github.com/anTuni/NomadCoin/blockchain"
-)
-
-const (
-	port        string = ":4000"
-	templateDIr string = "template/"
-)
-
-var templates *template.Template
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func add(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		templates.ExecuteTemplate(rw, "add", nil)
-	case "POST":
-		r.ParseForm()
-		data := r.Form.Get("blockData")
-		blockchain.GetBlockchain().AddBlock(data)
-		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
-	}
-
-}
-func home(rw http.ResponseWriter, r *http.Request) {
-	data := homeData{"Home", blockchain.GetBlockchain().AllBolocks()}
-	templates.ExecuteTemplate(rw, "home", data)
-}
+import "github.com/anTuni/NomadCoin/explorer"
 
 func main() {
-	templates = template.Must(template.ParseGlob(templateDIr + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDIr + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	explorer.Start()
 }
