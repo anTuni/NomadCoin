87f8acd085d35e5c3c648048df6d44cb888fd293
Tune <cfdw011@gmail.com>
2021-12-08 23:36:38 +0900

    암호화폐 마이닝

--- main.go
+++ main.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"strings"
+	"github.com/anTuni/NomadCoin/cli"
+	"github.com/anTuni/NomadCoin/db"
 )
 
 func main() {
-	difficulty := 5
-	target := strings.Repeat("0", difficulty)
-	nonce := 1
-	for {
-		hash := sha256.Sum256([]byte("string" + fmt.Sprint(nonce)))
-		fmt.Printf("Hash : %x\nTarget : %s\nNonce : %d\n", hash, target, nonce)
-		if strings.HasPrefix(fmt.Sprintf("%x", hash), target) {
-			return
-		} else {
-			nonce++
-		}
-
-	}
+	defer db.Close()
+	cli.Start()
 }
